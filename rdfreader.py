6ef35e30498e57a280100853b41e9b7b7a147a92
taufiq <taufiq1689@gmail.com>
2019-08-29 18:22:27 +0700

    task Rekayasa Pengetahuan done

--- rdfreader.py
+++ rdfreader.py
@@ -1,12 +1,11 @@
 import rdflib
 import xlsxwriter
-import sys
 url = "http://dbpedia.org/page/Rabbit"
 g=rdflib.Graph()
 g.parse(url)
 
 
-workbook = xlsxwriter.Workbook('.\Rabbit.xlsx')
+workbook = xlsxwriter.Workbook(r'.\Rabbit.xlsx')
 worksheet = workbook.add_worksheet()
 
 row = 0
@@ -16,6 +15,9 @@ worksheet.write(row, col,     'Subject')
 worksheet.write(row, col + 1, 'Predicate')
 worksheet.write(row, col + 2, 'Object')
 
+
+row = 1
+col = 0
 for s,p,o in g:
     s = s.split('/')
     p = p.split('/')
@@ -24,5 +26,12 @@ for s,p,o in g:
     print('this is subject: '+str(s[-1]))
     print('this is predicate: '+ str(p[-1]))
     print('this is object: '+ str(o[-1]))
+
+    worksheet.write(row, col,str(s[-1]))
+    worksheet.write(row, col + 1, str(p[-1]))
+    worksheet.write(row, col + 2, str(o[-1]))
+    
+    row+=1
+    
 
 workbook.close()
