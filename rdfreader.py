3fa5a923d79750665d739ab58d0bc8335d3ea2e1
taufiq <taufiq1689@gmail.com>
2019-08-29 18:17:38 +0700

    add create xlsx file in code

--- rdfreader.py
+++ rdfreader.py
@@ -1,8 +1,20 @@
 import rdflib
-
+import xlsxwriter
+import sys
 url = "http://dbpedia.org/page/Rabbit"
 g=rdflib.Graph()
 g.parse(url)
+
+
+workbook = xlsxwriter.Workbook('.\Rabbit.xlsx')
+worksheet = workbook.add_worksheet()
+
+row = 0
+col = 0
+
+worksheet.write(row, col,     'Subject')
+worksheet.write(row, col + 1, 'Predicate')
+worksheet.write(row, col + 2, 'Object')
 
 for s,p,o in g:
     s = s.split('/')
@@ -12,3 +24,5 @@ for s,p,o in g:
     print('this is subject: '+str(s[-1]))
     print('this is predicate: '+ str(p[-1]))
     print('this is object: '+ str(o[-1]))
+
+workbook.close()
